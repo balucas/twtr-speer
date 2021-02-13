2ffe8dec93ceefbfb803f0ba9d69963ea9d2e5bf
Bernard Lucas <blucas1597@gmail.com>
2021-02-13 18:57:21 -0500

    fixed registration bug

--- routes/users.js
+++ routes/users.js
@@ -23,14 +23,78 @@ router.post("/register", async (req, res, next) => {
     const newUser = db.user.build({
       username: username
     });
-    await newUser.generateHash(password);
-    await newUser.save();
-    
-    const data = {
-      "message": "user created" 
+    newUser.generateHash(password)
+      .then(() => {
+        const reg = newUser.save();
+        return reg;
+      })
+      .then((reg) => {
+        const data = {
+          "message": "user created",
+          "data": reg.id
+        }
+        res.status(200);
+        res.send(data);
+      })
+     .catch(error => {
+        const err = {
+          "error": "db_error", 
+          "message": "Database error",
+          "data": error
+        };
+        res.status(500);
+        res.send(err);
+    });
+  }
+})
+
+/* GET login user */
+router.get("/login", async (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  if (!username || !password) {
+    const err = {
+      "error": "missing_field", 
+      "message": "Missing field(s)"
+    };
+    res.status(400);
+    res.send(err);
+  } else {
+    try{
+      const user = await db.user.findOne({
+        where:{
+          username: username
+        }
+      });
+      
+      let status = 0;
+      let data = {};
+      
+      if (user == null) {
+        status = 404;
+        data = {
+          "message": "Username not found"
+        }
+      } else {
+        //user.validatePassword(password);
+        status = 200;
+        data = {
+          "message": "Success",
+          "data": user
+        }
+      }
+      res.status(status);
+      res.send(data);
+    } catch(error) {
+      const err = {
+        "error": "db_error", 
+        "message": "Database error",
+        "data": error
+      };
+      res.status(500);
+      res.send(err);
     }
-    res.status(200);
-    res.send(data);
   }
 })
 module.exports = router;
